d4dd4e57f6e7b7eb6795263b1d740f860df64334
Amar Jyoti Patra <amarjyotipatra511@gmail.com>
2025-03-06 21:26:13 +0530

    Razorpay api integrated

--- src/main/java/com/example/paymentserviceapplication/services/PaymentService.java
+++ src/main/java/com/example/paymentserviceapplication/services/PaymentService.java
@@ -1,12 +1,19 @@
 package com.example.paymentserviceapplication.services;
 
+import com.example.paymentserviceapplication.paymentgateways.RazorpayPaymentGateway;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PaymentService implements IPaymentService {
 
+    private RazorpayPaymentGateway razorpayPaymentGateway;
+
+    public PaymentService(RazorpayPaymentGateway razorpayPaymentGateway) {
+        this.razorpayPaymentGateway = razorpayPaymentGateway;
+    }
+
     @Override
     public String intiatePayment(String email,String name, String phoneNumber,String orderId, Long amount) {
-        return "";
+        return razorpayPaymentGateway.getPaymentLink(email,name,phoneNumber,orderId,amount);
     }
 }
