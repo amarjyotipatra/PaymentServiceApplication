0536401cbf8332b208e37a3d63bd3bd2b7b4d9d9
Amar Jyoti Patra <amarjyotipatra511@gmail.com>
2025-03-08 17:45:48 +0530

    webhook created at /stripeWebhook endpoint

---
+++ src/main/java/com/example/paymentserviceapplication/controllers/WebhookController.java
@@ -0,0 +1,16 @@
+package com.example.paymentserviceapplication.controllers;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebhookController {
+
+    @PostMapping("/stripeWebhook")
+    public String receiveEvents(@RequestBody String event) {
+        System.out.println(event);
+        return event;
+    }
+}
